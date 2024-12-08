9f4088915360fb90421bef62fd53e51d81095d80
Manuel Richard Popp <manuel.popp@wsl.ch>
2024-12-08 02:32:49 +0100

    Bug fix in response checking for cases the response is not a data frame.

--- R/check_response.R
+++ R/check_response.R
@@ -1,23 +1,26 @@
 #' Check response data
-#' 
+#'
 #' Check if the input data frame matches the required format. If the input is
 #' not a data frame, it will be replaced with an empty data frame. Missing
 #' columns are added and filled with NA.
-#' 
+#'
 #' @param x A data frame.
 #' @return A data frame matching the required format.
-#' 
+#'
 #' @export
 check_response <- function(x) {
   if (is.data.frame(x$data)) {
     data <- x$data
   } else {
-    print("Response data is not a dara.frame.")
-    data <- data.frame()
+    print("Response data is not a data.frame.")
+    data <- data.frame(matrix(ncol = 0, nrow = 1))
   }
-  
+
   missing <- setdiff(config$COLUMNS, names(data))
-  data[missing] <- NA
-  
+
+  for (column in missing) {
+    data[[column]] <- NA
+  }
+
   return(data)
 }
